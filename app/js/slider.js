4ef8f06ddfc1b75e618dbd221f06a1d52df4aded
Dmitri Davydov <Dmitri Davydov>
2017-03-13 16:56:41 +0100

    newslider working

--- app/js/slider.js
+++ app/js/slider.js
@@ -31,73 +31,82 @@
 }();
 
 // !function () {
-//
-//   function sliderNav(id, direction, state) {
-//     if(state === "on") {
-//       const nav = document.createElement('a');
-//       nav.className = "direct on";
-//       nav.id = id;
-//       nav.textContent = direction;
-//       return nav;
-//     } else {
-//       let nav = document.getElementById(id);
-//       nav.parentNode.removeChild(nav);
-//     }
-//   }
-//
+
 const modal = document.getElementById('modal');
 const modalImg = document.createElement("img");
+let prevImg,
+    nextImg,
+    img;
 
-function showModal(img) {
+function showPrevImg() {
+  modal.removeChild(modalImg);
+  modalImg.setAttribute("src", prevImg.getAttribute("src"));
+  modalImg.setAttribute("class", "front-pics active");
+  modal.appendChild(modalImg);
+  getPrevImg(prevImg);
+  getNextImg(prevImg);
+  console.log(prevImg);
+}
+
+function showNextImg() {
+  modal.removeChild(modalImg);
+  modalImg.setAttribute("src", nextImg.getAttribute("src"));
+  modalImg.setAttribute("class", "front-pics active");
+  modal.appendChild(modalImg);
+  getPrevImg(nextImg);
+  getNextImg(nextImg);
+  console.log(nextImg);
+}
+
+function getPrevImg(img) {
+  if(img.parentNode.previousElementSibling !== null) {
+    return prevImg = img.parentNode.previousElementSibling.firstElementChild;
+  } else {
+    hideModal();
+  }
+}
+function getNextImg(img) {
+  if(img.parentNode.nextElementSibling !== null) {
+    return nextImg = img.parentNode.nextElementSibling.firstElementChild;
+  } else {
+    hideModal();
+  }
+}
+
+function showModal() {
   modalImg.setAttribute("src", img.getAttribute("src"));
   modalImg.setAttribute("class", "front-pics active");
   modal.appendChild(modalImg);
   modal.style.opacity = 1;
   modal.style.zIndex = 9;
+  prevImg = getPrevImg(img);
+  nextImg = getNextImg(img);
+  console.log(prevImg);
 }
+
 function hideModal() {
   modal.style.opacity = 0;
   modal.style.zIndex = -1;
   modal.removeChild(modalImg);
 }
-function prevImg(img) {
-  console.log(img.parentNode.previousElementSibling);
-}
+
 document.addEventListener('click', function(e) {
-    let img = e.target;
-    if(img.tagName === 'IMG' && img.className==="front-pics" ) {
+    if(e.target.tagName === 'IMG' && e.target.className==="front-pics" ) {
+      img = e.target;
       showModal(img);
     }
     if(e.target.className === "close") {
       hideModal();
     }
-  if(e.target.className === "direct left") {
+  if(e.target.className === "direct left" || e.target.className === "direct right") {
 
     if(e.target.id === "left") {
-      prevImg(img);
-    } else {
-      changePic(img, "right");
+      showPrevImg();
+    } else if(e.target.id === "right") {
+      showNextImg();
     }
   }
 });
-// function changePic(img, direction) {
-//   img.className = "front-pics";
-//   if(direction === "left") {
-//     moveImg(img, "left");
-//   }
-//   if(direction === "right") {
-//     moveImg(img, "right");
-//   }
-// }
-// function moveImg(img, direction) {
-//
-//   if(direction === "left") {
-//
-//   }
-//   if(direction === "right") {
-//
-//   }
-// }
-//   }); //end event listener
+
 //
 // }();
